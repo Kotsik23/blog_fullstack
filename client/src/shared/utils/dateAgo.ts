c43de6f0eff6ff2ffc43c1e77cfe761951fcf0cb
oleg <olegsmushko@gmail.com>
2023-04-07 10:57:48 +0300

    FRONT: dateAgo function fix locale

--- client/src/shared/utils/dateAgo.ts
+++ client/src/shared/utils/dateAgo.ts
@@ -1,9 +1,11 @@
 import { formatDistanceToNow } from "date-fns"
-import { ru } from "date-fns/locale"
+import { ru, enUS } from "date-fns/locale"
+import i18next from "../../app/i18n"
 
 export const dateAgo = (date: Date) => {
+	const locale = i18next.language === "ru" ? ru : enUS
 	return formatDistanceToNow(date, {
 		addSuffix: true,
-		locale: ru,
+		locale,
 	})
 }
